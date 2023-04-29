5e258914452cb9e4bbcfa1db48901d45ec94fe6b
Solomon141 <31657654+Solomon141@users.noreply.github.com>
2023-04-29 11:02:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Logo from './logo/Badreg.jpg';
+import { Button, CheckBox } from 'react-bootstrap';
 
 function App() {
+  const [aggreed, setAgreed] = useState(false);
+
+  const registerUser = () => {
+    console.log('first');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', rowGap: 10 }}>
+            <img src={Logo} style={{ height: 120, width: 120 }} alt="logo" />
+            <input placeholder="Name" className="form-control" />
+            <input placeholder="Status" className="form-control" />
+            <input placeholder="Telephone" className="form-control" />
+            <input placeholder="Address" className="form-control" />
+            <input placeholder="Email" className="form-control" />
+
+            
+            <CheckBox />
+            <Button onClick={() => registerUser()}> REGISTER </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/common/BButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BButton() {
+  return (
+    <button>  </button>
+  )
+}
+
+export default BButton
